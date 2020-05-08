7b80c5ab739cf06153ec4ee807a0f2341fa3dcee
MizterB <5458030+MizterB@users.noreply.github.com>
2020-05-08 12:12:44 -0400

    Enable fan speed changes from the dashboard

--- custom_components/bond/fan.py
+++ custom_components/bond/fan.py
@@ -5,27 +5,25 @@ from homeassistant.components.fan import (
     SPEED_LOW,
     SPEED_MEDIUM,
     SPEED_HIGH,
-    FanEntity
+    FanEntity,
 )
 
-from bond import (
-    BOND_DEVICE_TYPE_CEILING_FAN,
-    BOND_DEVICE_ACTION_SET_SPEED
-)
+from bond import BOND_DEVICE_TYPE_CEILING_FAN, BOND_DEVICE_ACTION_SET_SPEED
 
 import logging
-DOMAIN = 'bond'
+
+DOMAIN = "bond"
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Bond Fan platform"""
-    bond = hass.data[DOMAIN]['bond_hub']
+    bond = hass.data[DOMAIN]["bond_hub"]
 
     for deviceId in bond.getDeviceIds():
         device = bond.getDevice(deviceId)
-        if device['type'] != BOND_DEVICE_TYPE_CEILING_FAN:
+        if device["type"] != BOND_DEVICE_TYPE_CEILING_FAN:
             continue
 
         deviceProperties = bond.getProperties(deviceId)
@@ -42,15 +40,15 @@ class BondFan(FanEntity):
         self._deviceId = deviceId
         self._device = device
         self._properties = properties
-        self._name = device['name']
+        self._name = device["name"]
         self._state = None
         self._speed_list = []
         self._speed_name_by_value = {}
         self._attributes = {}
 
-        if BOND_DEVICE_ACTION_SET_SPEED in self._device['actions']:
-            if 'max_speed' in self._properties:
-                self._speed_high = int(self._properties['max_speed'])
+        if BOND_DEVICE_ACTION_SET_SPEED in self._device["actions"]:
+            if "max_speed" in self._properties:
+                self._speed_high = int(self._properties["max_speed"])
                 self._speed_low = int(1)
                 self._speed_list.append(SPEED_LOW)
                 self._speed_name_by_value[self._speed_low] = SPEED_LOW
@@ -75,7 +73,7 @@ class BondFan(FanEntity):
     def speed_list(self) -> list:
         """Get the list of available speeds."""
         return self._speed_list
-    
+
     @property
     def device_state_attributes(self):
         """Return state attributes """
@@ -86,7 +84,7 @@ class BondFan(FanEntity):
         """Flag supported features."""
         supported_features = 0
 
-        if BOND_DEVICE_ACTION_SET_SPEED in self._device['actions']:
+        if BOND_DEVICE_ACTION_SET_SPEED in self._device["actions"]:
             supported_features |= SUPPORT_SET_SPEED
 
         return supported_features
@@ -94,10 +92,12 @@ class BondFan(FanEntity):
     def turn_on(self, speed=None, **kwargs):
         """Instruct the fan to turn on"""
         self._bond.turnOn(self._deviceId)
+        if speed is not None:
+            self.set_speed(speed)
 
     def turn_off(self, **kwargs):
         """Instruct the fan to turn off"""
-        self._attributes['current_speed'] = SPEED_OFF
+        self._attributes["current_speed"] = SPEED_OFF
         self._bond.turnOff(self._deviceId)
 
     def set_speed(self, speed: str) -> None:
@@ -108,19 +108,21 @@ class BondFan(FanEntity):
             self._bond.setSpeed(self._deviceId, self._speed_medium)
         elif speed == SPEED_LOW:
             self._bond.setSpeed(self._deviceId, self._speed_low)
-        self._attributes['current_speed'] = speed
+        self._attributes["current_speed"] = speed
 
     def update(self):
         """Fetch new state data for this fan
         This is the only method that should fetch new data for Home Assistant
         """
         bondState = self._bond.getDeviceState(self._deviceId)
-        if 'power' in bondState:
-            self._state = True if bondState['power'] == 1 else False
-            if self._state and bondState['speed'] in self._speed_name_by_value:
-                self._attributes['current_speed'] = self._speed_name_by_value[bondState['speed']]
+        if "power" in bondState:
+            self._state = True if bondState["power"] == 1 else False
+            if self._state and bondState["speed"] in self._speed_name_by_value:
+                self._attributes["current_speed"] = self._speed_name_by_value[
+                    bondState["speed"]
+                ]
             else:
-                self._attributes['current_speed'] = SPEED_OFF
+                self._attributes["current_speed"] = SPEED_OFF
 
     @property
     def unique_id(self):
